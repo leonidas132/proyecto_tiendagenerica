887d41a764f9ba2ddff8257b85a86b0c5ee993af
Moral <Moral@LAPTOP-06V9HDBE>
2021-09-28 14:07:22 -0500

    Se Sube Conexion a BBDD,

---
+++ src/main/java/controlador/Conexion.java
@@ -0,0 +1,28 @@
+package controlador;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class Conexion {
+	
+	Connection cnn;
+
+	public Connection conexionbd() {
+		
+		try {
+			Class.forName("com.mysql.cj.jdbc.Driver");
+			System.out.println("Conexion Ok");
+		} catch (ClassNotFoundException e) {
+			System.out.println("Error en Driver" + e);
+		}
+		try {
+			cnn=DriverManager.getConnection("jdbc:mysql://localhost/tienda", "root", "");
+			System.out.println("Conexion Ok");
+		} catch (SQLException e) {
+			System.out.println("Error con la URL" + e);
+		}
+		
+		return cnn;
+	}
+}
